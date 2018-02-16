66e2fa4e22dbfda3bd21651f6ce2bc2d9ac91890
Unknown <19tarigs@elmbrookstudents.org>
2018-02-16 12:01:51 -0600

    Test Class

---
+++ src/TestGreeter.java
@@ -0,0 +1,12 @@
+
+public class TestGreeter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+Greeter bob = new Greeter();
+
+System.out.println(bob.toString());
+	}
+
+}
